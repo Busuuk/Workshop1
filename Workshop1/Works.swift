ddec5573b70430ee1a9845e0f4804ea34fab639c
Simone Di Blasi <diblasisimone925@gmail.com>
2024-11-04 11:49:57 +0100

    bcwdhbkbaksh

---
+++ Workshop1/Works.swift
@@ -0,0 +1,18 @@
+//
+//  Works.swift
+//  Workshop1
+//
+//  Created by Simone Di Blasi on 04/11/24.
+//
+
+import SwiftUI
+
+struct Works: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Works()
+}
