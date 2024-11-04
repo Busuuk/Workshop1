47394271b126bc40feafb64c89853dc4323018c1
Simone Di Blasi <diblasisimone925@gmail.com>
2024-11-04 10:35:53 +0100

    Initial Commit

---
+++ Workshop1/Workshop1App.swift
@@ -0,0 +1,17 @@
+//
+//  Workshop1App.swift
+//  Workshop1
+//
+//  Created by Simone Di Blasi on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Workshop1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
